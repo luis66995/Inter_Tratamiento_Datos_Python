c259e672d2347ae8579689b7c8582ee84e6053c1
luis66995 <75743952+luis66995@users.noreply.github.com>
2022-06-30 13:07:51 -0500

    py

---
+++ listas3.py
@@ -0,0 +1,10 @@
+my_lista=[10,20,30,45,55,65,75,99,65,34]
+
+my_lista.insert(2,6)
+my_lista.append(45)
+my_lista.pop()
+print("El numero buscado esta en el elemento : ", my_lista.index(99))
+my_lista.index(99)
+print(my_lista)
+my_lista.remove(10)
+print("lista actualizada:", my_lista)
